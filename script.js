1af6a6245cb1ccc64553c5f3184a9237d94a8fa8
JEROME Emilien <jerome.emi@onja.org>
2020-08-08 09:51:49 +0300

    convert the index html and render the list from js into html

---
+++ script.js
@@ -0,0 +1,59 @@
+console.log('weekend project 6');
+
+// Add the object from readme
+
+const foods = [
+	{
+		id: 'ravitoto',
+		price: 5000,
+		title: 'Ravitoto',
+		spicy: true,
+		vegetarian: false,
+	},
+	{
+		id: 'pasta',
+		price: 4000,
+		title: 'Pasta',
+		spicy: true,
+		vegetarian: true,
+	},
+	{
+		id: 'burger',
+		price: 5000,
+		title: 'Burger',
+		spicy: false,
+		vegetarian: false,
+	},
+	{
+		id: 'rice',
+		price: 2000,
+		title: 'Rice and Leaves',
+		spicy: false,
+		vegetarian: true,
+	},
+	{
+		id: 'mofogasy',
+		price: 500,
+		title: 'Mofogasy',
+		spicy: false,
+		vegetarian: false,
+	},
+];
+
+// Get access to the ul element that has list class
+const list = document.querySelector('.list');
+
+// Map through the food lists and insert it into html 
+list.innerHTML = foods.map(item => 
+    `<li id="${item.id}">
+        <h3>${item.title}</h3>
+        <span>${item.price}</span>
+        <button>Add</button>
+    </li>`
+).join('');
+
+    
+
+
+
+
